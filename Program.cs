2d46d960d3fefc69934ffc427619a21b7793f865
Lozbeneva <luchandr@list.ru>
2022-10-03 09:11:59 +0300

    Added programm file

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+string[] source = {"Hello", "22", "world", ":-)"};
+int searchSize=3; //количество символов в строке для поиска. По условию задачи это 3 
+int newArraySize = 0; //счетчик количества удовлетворяющих условие элементов
+
+foreach (string element in source)
+{
+    if (element.Length <= searchSize)
+    {
+        newArraySize++;
+    }
+}
+
+if (newArraySize != 0)
+{
+    string[] destination = new string[newArraySize];
+    int k = 0;
+
+    foreach (string element in source)
+    {
+        if (element.Length <= searchSize)
+        {
+            destination[k] = element;
+            k++;
+        }
+    }
+    Console.Write(String.Join(",", destination));
+}
+else
+{
+    Console.WriteLine("В массиве нет удовлетворяющих условие элементов");
+}
